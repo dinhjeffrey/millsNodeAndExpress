a16f3cbea31c1407036a8b035dcd3916a22604f3
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-04-01 13:05:57 -0700

    added more node modules

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-![Copy](http://www.ruralking.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/a/j/ajax_lemon_dish_soap.jpg)# millsNodeAndExpress
+![Copy](http://www.ruralking.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/a/j/ajax_lemon_dish_soap.jpg)
---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp')
+
+var jsFiles = ['*.js', 'src/**/*.js']
+
+gulp.task('inject', function(){
+	var wiredep = require('wiredep').stream
+	var options = {
+		bowerJson: require('./bower.json'),
+		directory: './public/lib',
+		ignorePath: '../../public'
+	}
+
+	return gulp.src('./src/views/*.html')
+		.pipe(wiredep(options))
+		.pipe(gulp.dest('./src/views'))
+})
